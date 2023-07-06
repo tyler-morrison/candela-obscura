4aa4a08aa9da7c1b9c327e51e67d6eb1bf494280
Tyler Morrison <tmorrison@fastmail.com>
2023-07-06 17:42:26 -0400

    feat(data-access-circle): adds illumination logic

--- libs/data-access-circle/src/lib/+state/circle-machine.test.ts
+++ libs/data-access-circle/src/lib/+state/circle-machine.test.ts
@@ -1,8 +1,24 @@
-import { afterEach, beforeAll, beforeEach } from 'vitest';
-import { interpret, InterpreterFrom } from 'xstate';
+import {
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  test,
+} from 'vitest';
+import { InterpreterFrom, interpret } from 'xstate';
+
 import circleMachine from './circle-machine';
 
 let testActor: InterpreterFrom<typeof circleMachine>;
+
+function currentState() {
+  return testActor.getSnapshot().value;
+}
+
+function currentContext() {
+  return testActor.getSnapshot().context;
+}
 
 describe('Circle Machine', () => {
   beforeAll(() => {
@@ -17,8 +33,65 @@ describe('Circle Machine', () => {
     testActor.stop();
   });
 
+  // TODO: Stitch, Refresh, Train can only be done in-between assignments
   it('should work', () => {
-    let { value } = testActor.getSnapshot();
-    expect(value).toEqual('idle');
+    expect(currentState()).toEqual('idle');
+  });
+
+  describe('Circle Advancement', () => {
+    beforeAll(() => {
+      testActor = interpret(circleMachine);
+    });
+
+    beforeEach(() => {
+      testActor.send({ type: 'START_ASSIGNMENT' });
+      testActor.send({ type: 'COMPLETE_ASSIGNMENT' });
+    });
+
+    test('WHEN the assignment ends, THEN the GM should be able to update the circle’s illumination points', () => {
+      let getIllumination = () => currentContext().illumination;
+
+      expect(currentState()).toEqual({
+        debriefing: 'askIlluminationQuestions',
+      });
+      expect(getIllumination()).toEqual(0);
+
+      testActor.send({ type: 'UPDATE_ILLUMINATION.ADD', points: 1 });
+      expect(getIllumination()).toEqual(1);
+
+      testActor.send({ type: 'UPDATE_ILLUMINATION.REMOVE', points: 1 });
+      expect(getIllumination()).toEqual(0);
+
+      testActor.send({ type: 'UPDATE_ILLUMINATION.OVERRIDE', points: 23 });
+      expect(getIllumination()).toEqual(23);
+    });
+
+    test('WHEN the illumination track is full, THEN the track is cleared AND charachters advance', () => {
+      let getIllumination = () => currentContext().illumination;
+
+      // Start with a mostly full Illumination track
+      testActor.send({ type: 'UPDATE_ILLUMINATION.OVERRIDE', points: 23 });
+      expect(currentState()).toEqual({
+        debriefing: 'askIlluminationQuestions',
+      });
+      expect(getIllumination()).toEqual(23);
+
+      // The circle answers "Yes" to all Illumination Questions
+      testActor.send({ type: 'UPDATE_ILLUMINATION.ADD', points: 3 });
+      expect(getIllumination()).toEqual(26);
+
+      // Proceed to Illumination Key prompt
+      testActor.send({ type: 'NEXT' });
+      expect(currentState()).toEqual({ debriefing: 'checkIlluminationKeys' });
+
+      // Some, but not all, players fulfilled an Illumination Key during the assigment
+      testActor.send({ type: 'UPDATE_ILLUMINATION.ADD', points: 2 });
+      expect(getIllumination()).toEqual(28);
+      testActor.send({ type: 'NEXT' });
+
+      // Leftover Illumination (any points > 24) should be preserved
+      expect(getIllumination()).toEqual(4);
+      expect(currentState()).toEqual({ debriefing: 'selectingAdvancements' });
+    });
   });
 });
---
+++ libs/data-access-circle/src/lib/+state/models/events.model.ts
@@ -0,0 +1,13 @@
+export interface UpdateIlluminationPoints {
+  type:
+    | 'UPDATE_ILLUMINATION.ADD'
+    | 'UPDATE_ILLUMINATION.REMOVE'
+    | 'UPDATE_ILLUMINATION.OVERRIDE';
+  points: number;
+}
+
+export type CircleMachineEvents =
+  | { type: 'START_ASSIGNMENT' }
+  | { type: 'COMPLETE_ASSIGNMENT' }
+  | { type: 'NEXT' }
+  | UpdateIlluminationPoints;
