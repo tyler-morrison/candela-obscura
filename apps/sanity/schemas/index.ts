918740065bd487494435368458af313be1615c9a
Tyler Morrison <tmorrison@fastmail.com>
2023-08-15 22:28:31 -0400

    feat(sanity): creates a multi-workspace Studio configuration

---
+++ apps/sanity/schemas/index.ts
@@ -0,0 +1,7 @@
+import blockContent from './blockContent'
+import category from './category'
+import post from './post'
+import author from './author'
+
+export const blogSchemas = [post, author, category, blockContent]
+export const toolsSchemas = []
