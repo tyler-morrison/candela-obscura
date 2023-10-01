579c2a5bb8ae6920b70ba2fcfefb7d85a03294c7
Tyler Morrison <tmorrison@fastmail.com>
2023-09-30 21:16:45 -0400

    feat(machines-game-session): invokes basic action roll

--- libs/machines/game-session/src/lib/game-session.spec.ts
+++ libs/machines/game-session/src/lib/game-session.spec.ts
@@ -8,9 +8,15 @@ let testActor: Actor<typeof gameSessionMachine>;
 describe('Game Session Machine', () => {
   it('should work', () => {
     testActor = createActor(gameSessionMachine);
+    testActor.start();
+
+    testActor.send({ type: 'REQUEST_ROLL' });
+    testActor.send({ type: 'ROLL_DICE' });
 
     let current = testActor.getSnapshot();
+
     expect(current.value).toEqual('Idle');
+    expect(current.context.log.length).toEqual(1);
 
     testActor.stop();
   });
